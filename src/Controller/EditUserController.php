61623ad377ea53a3b0175579c9ee2dcb5cb3d2ce
Krzysztof Heigel <kfheigel@gmail.com>
2020-10-28 10:14:50 +0100

    Added possibility to edit entries in database

--- src/Controller/EditUserController.php
+++ src/Controller/EditUserController.php
@@ -2,23 +2,48 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\User;
+use App\Form\EditUserFormType;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class EditUserController extends AbstractController
 {
     /**
      * @Route("/edit/{id}", name="edit_user")
      */
-    public function index($id)
+    public function index(
+        $id,
+        Request $request,
+        UserPasswordEncoderInterface $passwordEncoder
+        )
     {
-        if($this->getUser()->getId()==$id){
-            return $this->render('edit_user/index.html.twig', [
-                'controller_name' => 'EditUserController',
-            ]);
-        }else{
+        if(!$this->getUser()->getId()==$id){
             return $this->redirectToRoute('index'); 
         }
-        
+
+        $em = $this->getDoctrine()->getManager();
+        $user =$em->getRepository(User::class)->find($id);
+
+        $form = $this->createForm(EditUserFormType::class, $user);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {            
+            $user->setCreatedAt(new \DateTime());
+            $user->setPassword(
+                $passwordEncoder->encodePassword(
+                    $user,
+                    $form->get('password')->getData()
+                )
+                );
+            $em->persist($user);
+            $em->flush();
+        }
+
+        return $this->render('edit_user/index.html.twig', [
+            'editUserForm' => $form->createView(),
+        ]);        
     }
 }
